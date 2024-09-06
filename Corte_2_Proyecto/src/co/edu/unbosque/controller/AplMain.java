ccb9f878e87e4c5d952b0f27efe231b89a4172b7
NICOLAS ABRIL <NICOLAS ABRIL@DESKTOP-KRKGS9T>
2024-09-06 10:28:04 -0500

    prueba 2

--- Corte_2_Proyecto/src/co/edu/unbosque/controller/AplMain.java
+++ Corte_2_Proyecto/src/co/edu/unbosque/controller/AplMain.java
@@ -3,7 +3,7 @@ package co.edu.unbosque.controller;
 public class AplMain {
 
 	public static void main(String[] args) {
-	System.out.println("si sirvio");
+	System.out.println("esto es commit de prueba 2");
 
 	}
 
