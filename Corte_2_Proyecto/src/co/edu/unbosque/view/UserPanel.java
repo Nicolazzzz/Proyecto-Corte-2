2c771a0e191f95c360f027eb7e973dc22dfdc85d
Familia <Familia@RodrAvilHP>
2024-09-24 22:53:24 -0500

    vot

--- Corte_2_Proyecto/src/co/edu/unbosque/view/UserPanel.java
+++ Corte_2_Proyecto/src/co/edu/unbosque/view/UserPanel.java
@@ -1,5 +1,18 @@
 package co.edu.unbosque.view;
 
-public class UserPanel {
-//a
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class UserPanel extends JPanel {
+
+	private JTextField destinoField;
+	private JButton volverBtn, buscarBtn;
+
+	public UserPanel() {
+		setSize(1280, 720);
+		setLayout(null);
+		setVisible(true);
+
+	}
 }
