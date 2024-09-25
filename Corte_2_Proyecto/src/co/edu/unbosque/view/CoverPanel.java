86f762176106794c5c388a253bc3e6e1acc569d0
Familia <Familia@RodrAvilHP>
2024-09-24 22:34:21 -0500

    panel

---
+++ Corte_2_Proyecto/src/co/edu/unbosque/view/CoverPanel.java
@@ -0,0 +1,7 @@
+package co.edu.unbosque.view;
+
+import javax.swing.JPanel;
+
+public class CoverPanel extends JPanel {
+
+}
---
+++ Corte_2_Proyecto/src/co/edu/unbosque/view/InputPanel.java
@@ -0,0 +1,7 @@
+package co.edu.unbosque.view;
+
+import javax.swing.JPanel;
+
+public class InputPanel extends JPanel{
+
+}
